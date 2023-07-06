702fba3766902a582f999c3d811b296c0422b109
AMPLE <wnfjq9397@naver.com>
2023-07-06 11:50:35 +0900

    2023_07_06_commit_study_ver_1.0

---
+++ public/en/pc/dist/javascripts/design_common.js
@@ -0,0 +1,30 @@
+"use strict";
+var winW;
+var winH;
+var esStep = "Expo.ease";
+var esOut = "Expo.easeOut";
+var $window = $(window);
+var winSc = $(window).scrollTop();
+
+$window.load(function () {
+    var _this =  $(this);
+    winW = _this.width();
+    winH = _this.height();
+    winSc = _this.scrollTop();
+    $window.on("resize", function () {
+        winW = _this.width();
+        winH = _this.height();
+    });
+    _this.trigger("resize");
+    $(window).scroll(function () {
+        winSc = _this.scrollTop();
+    });
+    layout();
+    main();
+});
+function layout() {
+
+}
+function main() {
+
+}
